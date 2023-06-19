8c0a43671f2d6aaa27048a6b644f335b67a449cf
ivanbeltram <ivan97beltram@gmail.com>
2023-06-19 02:38:57 -0300

    Copia rama main

---
+++ cancha-futbol/src/views/HomeView.vue
@@ -0,0 +1,39 @@
+
+  <template>
+  <div>
+      <SlideShow/>
+  </div>
+  <div>
+      <PasosUso/>
+  </div>
+
+  </template>
+  <script>
+import SlideShow from '../components/SlideShow.vue';
+import PasosUso from '../components/PasosUso.vue';
+
+    export default {
+      name: 'HomeView',
+      components: {
+        SlideShow,
+        PasosUso
+  },
+      data() {
+        return {
+          paginas: [
+             {
+              "nombre": "Home",
+              "id": "1",
+              "url": "https://randomuser.me/api/portraits/thumb/women/9.jpg"
+             },
+             {
+                "nombre": "Contacto",
+              "id": "2",
+              "url": "https://randomuser.me/api/portraits/thumb/women/9.jpg"
+             }
+          ]
+        }
+      }
+      
+    }
+  </script>
